cc7b34b4e92ca2a3dcbca31259bfdd93ecf47dbc
buingoclam147 <buingoclam00@gmail.com>
2021-09-19 09:35:01 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endPoint: 'https://dbvlxd.herokuapp.com/',
-  localhost: 'https://dbvlxd.herokuapp.com/api/'
+  endPoint: 'https://dbvlxd.herokuapp.com/api/',
+  localhost: 'https://dbvlxd.herokuapp.com/'
 };
